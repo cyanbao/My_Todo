3f89188a6f6da4e83931ce5e16cb486925464e17
cyanbao <1021834777@qq.com>
2017-04-24 21:03:54 +0800

    提交本地全部文档

---
+++ www/js/controllers/TasksController.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers')
+.controller('TasksController', function ($rootScope, $scope, Tasks) {
+	
+	$scope.tasks = Tasks.all();
+	$scope.complete = function(task){
+
+  	var temp = task;
+  	Tasks.delete(task);
+  	//Tasks.remove(task);
+  	temp.st = "text-decoration:line-through" ;
+  	Tasks.save(temp);
+  	$scope.tasks = Tasks.all();
+
+  };
+
+});
